d0332865a35a320d7b6cfd2663471e4932071f05
Vova <vovakulikov@icloud.com>
2017-02-12 01:46:55 +0300

    Добавляет функцию выбора нужного пикселя из массива ImageData

--- src/app.js
+++ src/app.js
@@ -12,9 +12,18 @@ let handler = (evt)=>{
 
 function genHandler(evt){
     let info = getInfoCanvas(canvas);
+    console.log(info.data[info.data.length-4],info.data[info.data.length-3],info.data[info.data.length-2],info.data[info.data.length-1])
+    console.log(info.data.length)
     let filter = new Filters(info);
-    console.log(info.data[0])
-    info = filter.convertToGray().getImageData();
+    console.log(info.data[(info.width*info.height)*4-4],info.data[(info.width*info.height)*4-3],info.data[(info.width*info.height)*4-2],info.data[(info.width*info.height)*4-1])
+    console.log(info.data[0],info.data[1],info.data[2],info.data[3])
+    let infoAboutPixel = filter.getCurrentPixel(0,0);
+    console.log('Info about pixel',infoAboutPixel)
+    info = filter
+            .invert()
+            .convertToGray()
+            .getImageData();
+
     draw(canWin,info,'paintPixels');
 }
 
--- src/filters.js
+++ src/filters.js
@@ -4,6 +4,8 @@ export default class Filters{
         this._data = imageData.data;
         this._widthImage = imageData.width;
         this._heightImage = imageData.height;
+
+        console.log('Информация из конструктора ширина и высота', this._widthImage, this._heightImage)
     }
 
     convertToGray(){
@@ -19,6 +21,34 @@ export default class Filters{
         }
 
         return this;
+    }
+    invert(){
+        let imageLength = this._widthImage *  this._heightImage * 4;
+        let d = this._data;
+
+        for(let i = 0; i < imageLength; i+= 4){
+            let red = d[i],
+                green = d[i+1],
+                blue = d[i+2];
+
+            d[i] = 255 - red;
+            d[i+1] = 255 - green;
+            d[i+2] = 255 - blue;
+        }
+
+        return this;
+
+    }
+    sobelOperator(){
+        let imageLength = this._widthImage *  this._heightImage * 4;
+        let d = this._data;
+
+
+    }
+    getCurrentPixel(x,y){
+        var correctPixel = (x+y*this._widthImage)*4;
+        let d = this._data;
+        return [d[correctPixel],d[correctPixel+1],d[correctPixel+2],d[correctPixel+3]]
     }
     getImageData(){
         return this.imageData
