8d63251df944128d7f91d8e7ae866ae0be72ea46
Vova <vovakulikov@icloud.com>
2017-02-24 14:16:08 +0300

    Добавляет функцию closest native DOM

--- src/helpers.js
+++ src/helpers.js
@@ -117,3 +117,15 @@ export function getInfoCanvas(canvas){
     return ctx.getImageData(0,0,canvas.width,canvas.height);
 }
 
+
+export function closest(el, selector) {
+    var matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+
+    while (el) {
+        if (matchesSelector.call(el, selector)) {
+            break;
+        }
+        el = el.parentElement;
+    }
+    return el;
+}
