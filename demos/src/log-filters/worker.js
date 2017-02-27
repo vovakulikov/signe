6e3f41b452c5bd5012e166e6d945c738f56f3efa
Vova <vovakulikov@icloud.com>
2017-02-28 00:01:30 +0300

    Добавляет web worker для обработки изображения

--- demos/src/log-filters/worker.js
+++ demos/src/log-filters/worker.js
@@ -1,12 +1,22 @@
 /**
  * Created by Vova on 27.02.2017.
  */
-
+import Filters from '../../../src/filters.js';
 self.onmessage = function(e) {
-    console.log('Message received from main script');
-    var workerResult = 'Result: ' + (e.data);
-    console.log('Posting message back to main script');
-    console.log(e)
-    self.postMessage(workerResult);
-    console.log(e.type)
+    //console.log(e.data)
+    console.log(e.data.func)
+    switch(e.data.func){
+
+        case ('processingImage'):{
+            let filter = new Filters(e.data.infoPixel);
+            filter.convertToGray().LoGfilter();
+
+            self.postMessage({
+                'resposne': e.data.infoPixel
+            });
+            break;
+        }
+    }
+    //self.postMessage('hellloo wolrdddd');
+    //console.log(e.type)
 }
